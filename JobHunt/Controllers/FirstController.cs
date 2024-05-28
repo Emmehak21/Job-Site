5b1bac354551f65e14c2b0a2a3c0023d58dfce53
ADMIN <ADMIN@DESKTOP-MO53A58>
2024-05-28 22:55:06 +0500

    Add project files.

---
+++ JobHunt/Controllers/FirstController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobHunt.Controllers
+{
+    public class FirstController : Controller
+    {
+      
+        public IActionResult home()
+        {
+            return View();
+        }
+
+
+    }
+}
