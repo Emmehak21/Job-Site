fbab4626dde434c03ca7c4211c941849dad5f363
ADMIN <ADMIN@DESKTOP-MO53A58>
2024-06-05 15:37:14 +0500

    layout changes

--- JobHunt/Controllers/FirstController.cs
+++ JobHunt/Controllers/FirstController.cs
@@ -6,7 +6,36 @@ namespace JobHunt.Controllers
     {
       
         public IActionResult home()
+
         {
+            ViewData["Title"] = "Home";
+            ViewBag.Quote = "Your Dream Job Awaits!";
+            TempData["Quote2"] = "Discover thousands of job opportunities tailored to your skills and ambitions.\r\nJoin now to find the perfect match and take the next step in your career journey.";
+            return View();
+        }
+
+        public IActionResult job()
+
+        {
+            ViewData["Title"] = "Find jobs";
+            return View();
+        }
+        public IActionResult Candidates()
+
+        {
+            ViewData["Title"] = "Find Candidates";
+            return View();
+        }
+        public IActionResult blog()
+
+        {
+            ViewData["Title"] = "Blog";
+            return View();
+        }
+        public IActionResult ContactUs()
+
+        {
+            ViewData["Title"] = "Contact Us";
             return View();
         }
 
