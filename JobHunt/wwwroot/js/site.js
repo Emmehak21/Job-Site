959cadb38b120dee393e176c176fdfc9fda11517
ADMIN <ADMIN@DESKTOP-MO53A58>
2024-06-12 16:13:49 +0500

    commit3

--- JobHunt/wwwroot/js/site.js
+++ JobHunt/wwwroot/js/site.js
@@ -1,10 +1,3 @@
 ﻿
-document.querySelectorAll('.read-more').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const post = link.parentNode;
-        const content = post.querySelector('.post-content');
-        content.classList.toggle('show');
-    });
-});
+
 
